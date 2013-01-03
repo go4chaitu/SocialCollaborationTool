75082ba0879e4da37ad0f8569b4c48990cdcef36
Chaitanya Saragadam <go4chaitu@gmail.com>
2013-01-03 08:03:14 +0530

    changed
    aldsjflkadsf

--- src/js/textbox.js
+++ src/js/textbox.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){    
      
     document.getElementById("box").contentEditable='true';
+    $('#box').addClass("placeholder");
+    
     $('#box').click(function(){
     	if ($(this).hasClass("placeholder"))                
     	{      		          
@@ -22,6 +24,60 @@ $(document).ready(function(){
     } 
     
     $( "#circle" ).draggable();    
-    $('#container').layout();
+    $( '#container').layout();    
+    $('#savedComments').accordion({ 
+    	header: '.ui-accordion-header', 
+    	event:'mouseover' 
+    	});    
+    /*$('.tagsNonEdit').openDOMWindow({ 
+    	eventType:'click', 
+    	loader:0, 
+    	windowBGColor:'#aaa', 
+    	loaderHeight:12, 
+    	loaderWidth:12,
+    	windowSourceID:'urlpath'
+    	});*/ 
+    $('.tagsNonEdit').popupWindow({ 
+    	location:0,
+    	menubar:1,
+    	height:400, 
+		width:400, 
+		top:200, 
+		left:400,		
+		windowURL:"tags.html",
+		windowName:"This is my popup window"
+    });    
+    
+    if( $("#tags").text().length > 40 )
+    {
+    	$( "#tags" ).html( $("#tags").text().substring(0,40) + "...");
+	}
+    else
+    {
+    	$( "#tags" ).html( $("#tags").text() );
+    } 
+    
+    $("#tags").mouseover(function () 
+			   { 
+		   $(this).addClass("tagsEdit");	
+		   $(this).removeClass("tagsNonEdit");
+		   $(".tagspopup").attr("href","#inlineContent");
+	   }).mouseout(function(){
+		   $(this).addClass("tagsNonEdit");
+		   $(this).removeClass("tagsEdit");
+		   if( $("#tags").text().length > 40 )
+		    {
+		    	$( "#tags" ).html( $("#tags").text().substring(0,40) + "...");
+			}
+		    else
+		    {
+		    	$( "#tags" ).html( $("#tags").text() );
+		    } 		    
+		});
+    
+    $("#tags").click(function ()
+    		{
+    			
+    		});       
 
   });
--- src/js/toolbar.js
+++ src/js/toolbar.js
@@ -32,6 +32,27 @@
 			icons: {
 				primary: "ui-icon-circle-plus"
 			}
+		}).click(function() {			
+			if( !$("#box").hasClass("placeholder") )
+			{				
+				/*$("#savedComments").prepend("<div id=\"savedBox\" class=\"savedComment savedCommentMouseOut\">"+$("#box").text()+"</div>");
+			    $( '.savedComment' ).mouseover(function () 
+						   { 
+							   $(this).addClass("savedCommentMouseIn");	
+							   $(this).removeClass("savedCommentMouseOut");
+						   }).mouseout(function(){
+							   $(this).addClass("savedCommentMouseOut");
+							   $(this).removeClass("savedCommentMouseIn");
+							});*/
+				$("#savedComments").prepend("<div class=\"ui-accordion-group\"><div class=\"ui-accordion-header\"><a href=\"#\">panel 1</a></div><div class=\"ui-accordion-content\" style=\"height: 100px;\"><div style=\"padding:10px;\">My Contents</div></div></div>");
+				$("#savedComments").accordion({ 
+			    	header: '.ui-accordion-header', 
+			    	event:'mouseover' 
+			    	}); 
+				$("#box").html($("#box").data("placeholder"));           
+	            $("#box").addClass("placeholder");          
+				
+			}			
 		});
 		$( "#delete" ).button({
 			text: false,
